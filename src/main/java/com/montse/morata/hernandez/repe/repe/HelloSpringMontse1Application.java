6b470190f155a82ef1ea9cd22c0e6ad434f3286a
Montse Morata <sinaimh@gmail.com>
2024-09-09 12:17:54 +0200

    primrecomit

---
+++ src/main/java/com/montse/morata/hernandez/repe/repe/HelloSpringMontse1Application.java
@@ -0,0 +1,13 @@
+package com.montse.morata.hernandez.repe.repe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringMontse1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringMontse1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/montse/morata/hernandez/repe/repe/HelloSpringMontse1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montse.morata.hernandez.repe.repe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringMontse1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
